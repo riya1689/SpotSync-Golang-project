46547930ca7d6eca7927ef82ff672b5fd14231b5
riya1689 <riyaratri24@gmail.com>
2026-06-28 15:38:22 +0600

    fix: connecting database

---
+++ cmd/server/main.go
@@ -0,0 +1,14 @@
+package main
+import(
+	"log"
+	"spotSync-golang-Project/config"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	config.ConnectDB()
+}  
