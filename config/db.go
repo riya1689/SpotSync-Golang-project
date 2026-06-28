87324af783f7304a7ea24f566b41a942ba909263
riya1689 <riyaratri24@gmail.com>
2026-06-28 15:06:14 +0600

    feat: initialize database connection

---
+++ config/db.go
@@ -0,0 +1,30 @@
+package config
+
+import (
+	"database/sql"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func ConnectDB() {
+	dsn := os.Getenv("DATABASE_URL")
+	if dsn == "" {
+		log.Fatal("DATABASE_URL environment variable is not set")
+	}
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal("Failed to connect to database:", err)
+	}
+
+	sql.SetMaxIdleConns(10)
+	sql.SetMaxOpenConns(100)
+
+	DB = db
+	log.Println("Database connected successfully")
+}
