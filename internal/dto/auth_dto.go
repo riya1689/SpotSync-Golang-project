6b68d9e2c3e754ab2856e7bbc828c057f68e3a1a
riya1689 <riyaratri24@gmail.com>
2026-06-28 18:16:47 +0600

    feat: added auth DTO

---
+++ internal/dto/auth_dto.go
@@ -0,0 +1,31 @@
+package dto
+
+import "time" 
+
+type RegisterRequest struct { 
+	Name     string `json:"name" validate:"required"` 
+	Email    string `json:"email" validate:"required,email"` 
+	Password string `json:"password" validate:"required,min=6"` 
+	Role     string `json:"role" validate:"omitempty,oneof=driver admin"` 
+}
+
+type AuthUserResponse struct { 
+	ID        uint      `json:"id"` 
+	Name      string    `json:"name"` 
+	Email     string    `json:"email"`
+	Role      string    `json:"role"` 
+	CreatedAt time.Time `json:"created_at,omitempty"`
+	UpdatedAt time.Time `json:"updated_at,omitempty"` 
+}
+
+type LoginRequest struct {
+	Email    string `json:"email" validate:"required,email"`
+	Password string `json:"password" validate:"required"` 
+}
+
+
+
+type LoginResponse struct { 
+	Token string           `json:"token"` 
+	User  AuthUserResponse `json:"user"` 
+}
